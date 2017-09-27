ab17426d91919b63093704a54aa75e1e57ae36d5
Prateek Gogia <gogia.prateek@hotmail.com>
2017-09-28 01:18:42 +0530

    Changed the Layout of Profile Page.

--- game-shot/routes/user.js
+++ game-shot/routes/user.js
@@ -5,6 +5,8 @@ var expressValidator = require('express-validator');
 router.use(expressValidator());
 var passport = require('passport');
 var User = require('../models/user');
+var Order = require('../models/order');
+var Cart = require('../models/cart');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -30,11 +32,16 @@ router.get('/signin', function (req, res) {
 });
 
 router.get('/profile', function (req, res) {
-    User.find({}, function (err, email) {
-        res.render('user/profile', {
-            username: req.user.firstname,
-            date: req.user.createdOn
+Order.find({user: req.user}, function(err, orders){
+    if(err) {
+        return res.write('Error!');
+    }
+    var cart;
+    orders.forEach(function(order) {
+        cart = new Cart(order.cart);
+        order.items = cart.generateArray();
         });
+    res.render('user/profile', {orders: orders});
     });
 });
 router.post('/signin', passport.authenticate('local.signin', {
@@ -51,7 +58,7 @@ router.post('/signin', passport.authenticate('local.signin', {
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-    failureRedirect: 'user/signup',
+    failureRedirect: '/user/signup',
     failureFlash: true
 }), function (req, res, next) {
     if (req.session.oldUrl) {
